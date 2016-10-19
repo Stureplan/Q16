3f7b468375c20a2cbb441ff7395fcaf80321fa10
Stureplan <linus.ax@gmail.com>
2016-10-19 03:03:51 +0200

    Improved all gibs

---
+++ Assets/Q16/Code/ThrowGibs.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowGibs : MonoBehaviour
+{
+    Rigidbody[] rbs;
+
+    float bForce;
+    float uForce;
+
+    void Start ()
+    {
+
+        rbs = GetComponentsInChildren<Rigidbody>();
+
+        for (int i = 0; i < rbs.Length; i++)
+        {
+            bForce = Random.Range(1.0f, 7.5f);
+            uForce = Random.Range(1.0f, 7.5f);
+
+            rbs[i].AddForce(-rbs[i].transform.forward * bForce, ForceMode.Impulse);
+            rbs[i].AddForce(Vector3.up * uForce, ForceMode.Impulse);
+            rbs[i].AddTorque(Utility.RandomVector3() * 5.0f, ForceMode.Impulse);
+        }
+
+
+	}
+}

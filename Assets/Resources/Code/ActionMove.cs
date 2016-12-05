6d963515866bb137cd5271bc58e3ae86704848be
Stureplan <linus.ax@gmail.com>
2016-12-05 19:50:38 +0100

    Gate continued; Separate Move Action, Objective Action

---
+++ Assets/Resources/Code/ActionMove.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionMove : ActionObject
+{
+    public Move move;
+
+
+    public override void Action(SenderInfo sender)
+    {
+        Move();
+    }
+
+
+
+    void Move()
+    {
+        StartCoroutine(IEMove(move.transformToMove.position + move.toPosition, move.duration));
+    }
+
+    IEnumerator IEMove(Vector3 to, float time)
+    {
+        float t = 0.0f;
+        float d = Vector3.Distance(move.transformToMove.position, to);
+
+        while (t < time)
+        {
+            float delta = (d * Time.deltaTime) / time;
+
+            move.transformToMove.position = Vector3.MoveTowards(move.transformToMove.position, to, delta);
+            t += Time.deltaTime;
+
+            yield return null;
+        }
+
+        //done
+    }
+}
---
+++ Assets/Resources/Code/ActionPartialObjective.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionPartialObjective : ActionObject
+{
+    public ActionObject actionWhenFinished;
+    public string finishMessage;
+    public int objectivesToComplete;
+
+
+    private int objectivesDone = 0;
+    
+    public override void Action(SenderInfo sender)
+    {
+        objectivesDone++;
+
+
+        if (objectivesDone >= objectivesToComplete)
+        {
+            actionWhenFinished.Action(sender);
+            MessageLog.Message(finishMessage);
+        }
+        else
+        {
+            MessageLog.Message((objectivesToComplete - objectivesDone).ToString() + " more to go");
+        }
+    }
+}
